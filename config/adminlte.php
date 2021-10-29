4d36bdf5f790189445029a7e0b58dc415dded243
Eduardo Cauich Herrera <lalo_lego@hotmail.com>
2021-10-29 14:01:43 -0500

    Cambios en los packages
    Se cambiaron las versiones de los packages que causaban conflicto con intelephense, como tambien idiomas que no se usan de adminlte y log-viewer

--- config/adminlte.php
+++ config/adminlte.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Title
 	|--------------------------------------------------------------------------
@@ -13,11 +13,11 @@ return [
 	|
 	*/
 
-	'title' => 'AdminLTE 3',
-	'title_prefix' => '',
-	'title_postfix' => ' | PcKonect',
+    'title' => 'AdminLTE 3',
+    'title_prefix' => '',
+    'title_postfix' => ' | PcKonect',
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Favicon
 	|--------------------------------------------------------------------------
@@ -29,10 +29,10 @@ return [
 	|
 	*/
 
-	'use_ico_only' => true,
-	'use_full_favicon' => false,
+    'use_ico_only' => true,
+    'use_full_favicon' => false,
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Logo
 	|--------------------------------------------------------------------------
@@ -44,14 +44,14 @@ return [
 	|
 	*/
 
-	'logo' => '<b>Pc</b>Konect',
-	'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
-	'logo_img_class' => 'brand-image img-circle elevation-3',
-	'logo_img_xl' => null,
-	'logo_img_xl_class' => 'brand-image-xs',
-	'logo_img_alt' => 'AdminLTE',
+    'logo' => '<b>Pc</b>Konect',
+    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo_img_class' => 'brand-image img-circle elevation-3',
+    'logo_img_xl' => null,
+    'logo_img_xl_class' => 'brand-image-xs',
+    'logo_img_alt' => 'AdminLTE',
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| User Menu
 	|--------------------------------------------------------------------------
@@ -63,14 +63,14 @@ return [
 	|
 	*/
 
-	'usermenu_enabled' => true,
-	'usermenu_header' => false,
-	'usermenu_header_class' => 'bg-primary',
-	'usermenu_image' => false,
-	'usermenu_desc' => false,
-	'usermenu_profile_url' => false,
+    'usermenu_enabled' => true,
+    'usermenu_header' => false,
+    'usermenu_header_class' => 'bg-primary',
+    'usermenu_image' => false,
+    'usermenu_desc' => false,
+    'usermenu_profile_url' => false,
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Layout
 	|--------------------------------------------------------------------------
@@ -82,13 +82,13 @@ return [
 	|
 	*/
 
-	'layout_topnav' => null,
-	'layout_boxed' => null,
-	'layout_fixed_sidebar' => null,
-	'layout_fixed_navbar' => null,
-	'layout_fixed_footer' => null,
+    'layout_topnav' => null,
+    'layout_boxed' => null,
+    'layout_fixed_sidebar' => null,
+    'layout_fixed_navbar' => null,
+    'layout_fixed_footer' => null,
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Authentication Views Classes
 	|--------------------------------------------------------------------------
@@ -100,14 +100,14 @@ return [
 	|
 	*/
 
-	'classes_auth_card' => 'card-outline card-primary',
-	'classes_auth_header' => '',
-	'classes_auth_body' => '',
-	'classes_auth_footer' => '',
-	'classes_auth_icon' => '',
-	'classes_auth_btn' => 'btn-flat btn-primary',
+    'classes_auth_card' => 'card-outline card-primary',
+    'classes_auth_header' => '',
+    'classes_auth_body' => '',
+    'classes_auth_footer' => '',
+    'classes_auth_icon' => '',
+    'classes_auth_btn' => 'btn-flat btn-primary',
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Admin Panel Classes
 	|--------------------------------------------------------------------------
@@ -119,19 +119,19 @@ return [
 	|
 	*/
 
-	'classes_body' => '',
-	'classes_brand' => '',
-	'classes_brand_text' => '',
-	'classes_content_wrapper' => '',
-	'classes_content_header' => '',
-	'classes_content' => '',
-	'classes_sidebar' => 'sidebar-dark-primary elevation-4',
-	'classes_sidebar_nav' => '',
-	'classes_topnav' => 'navbar-white navbar-light',
-	'classes_topnav_nav' => 'navbar-expand',
-	'classes_topnav_container' => 'container',
+    'classes_body' => '',
+    'classes_brand' => '',
+    'classes_brand_text' => '',
+    'classes_content_wrapper' => '',
+    'classes_content_header' => '',
+    'classes_content' => '',
+    'classes_sidebar' => 'sidebar-dark-primary elevation-4',
+    'classes_sidebar_nav' => '',
+    'classes_topnav' => 'navbar-white navbar-light',
+    'classes_topnav_nav' => 'navbar-expand',
+    'classes_topnav_container' => 'container',
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Sidebar
 	|--------------------------------------------------------------------------
@@ -143,17 +143,17 @@ return [
 	|
 	*/
 
-	'sidebar_mini' => true,
-	'sidebar_collapse' => false,
-	'sidebar_collapse_auto_size' => false,
-	'sidebar_collapse_remember' => false,
-	'sidebar_collapse_remember_no_transition' => true,
-	'sidebar_scrollbar_theme' => 'os-theme-light',
-	'sidebar_scrollbar_auto_hide' => 'l',
-	'sidebar_nav_accordion' => true,
-	'sidebar_nav_animation_speed' => 300,
+    'sidebar_mini' => true,
+    'sidebar_collapse' => false,
+    'sidebar_collapse_auto_size' => false,
+    'sidebar_collapse_remember' => false,
+    'sidebar_collapse_remember_no_transition' => true,
+    'sidebar_scrollbar_theme' => 'os-theme-light',
+    'sidebar_scrollbar_auto_hide' => 'l',
+    'sidebar_nav_accordion' => true,
+    'sidebar_nav_animation_speed' => 300,
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Control Sidebar (Right Sidebar)
 	|--------------------------------------------------------------------------
@@ -165,15 +165,15 @@ return [
 	|
 	*/
 
-	'right_sidebar' => false,
-	'right_sidebar_icon' => 'fas fa-cogs',
-	'right_sidebar_theme' => 'dark',
-	'right_sidebar_slide' => true,
-	'right_sidebar_push' => true,
-	'right_sidebar_scrollbar_theme' => 'os-theme-light',
-	'right_sidebar_scrollbar_auto_hide' => 'l',
+    'right_sidebar' => false,
+    'right_sidebar_icon' => 'fas fa-cogs',
+    'right_sidebar_theme' => 'dark',
+    'right_sidebar_slide' => true,
+    'right_sidebar_push' => true,
+    'right_sidebar_scrollbar_theme' => 'os-theme-light',
+    'right_sidebar_scrollbar_auto_hide' => 'l',
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| URLs
 	|--------------------------------------------------------------------------
@@ -185,16 +185,16 @@ return [
 	|
 	*/
 
-	'use_route_url' => false,
-	'dashboard_url' => 'home',
-	'logout_url' => 'logout',
-	'login_url' => 'login',
-	'register_url' => 'register',
-	'password_reset_url' => 'password/reset',
-	'password_email_url' => 'password/email',
-	'profile_url' => false,
+    'use_route_url' => false,
+    'dashboard_url' => 'home',
+    'logout_url' => 'logout',
+    'login_url' => 'login',
+    'register_url' => 'register',
+    'password_reset_url' => 'password/reset',
+    'password_email_url' => 'password/email',
+    'profile_url' => false,
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Laravel Mix
 	|--------------------------------------------------------------------------
@@ -206,11 +206,11 @@ return [
 	|
 	*/
 
-	'enabled_laravel_mix' => false,
-	'laravel_mix_css_path' => 'css/app.css',
-	'laravel_mix_js_path' => 'js/app.js',
+    'enabled_laravel_mix' => false,
+    'laravel_mix_css_path' => 'css/app.css',
+    'laravel_mix_js_path' => 'js/app.js',
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Menu Items
 	|--------------------------------------------------------------------------
@@ -222,122 +222,80 @@ return [
 	|
 	*/
 
-	'menu' => [
-		['header' => 'main_navigation'],
-		[
-			'text' => 'Dashboard',
-			'url' => 'admin',
-			'icon' => 'nav-icon fas fa-tachometer-alt',
-			'label_color' => 'success',
-		],
-		['header' => 'administration'],
-		[
-			'text' => 'Usuarios',
-			'route' => 'admin.users.index',
-			'icon' => 'nav-icon fas fa-user-friends',
-			'label_color' => 'success',
-		],
-		[
-			'text' => 'Roles',
-			'route' => 'admin.roles.index',
-			'icon' => 'nav-icon fas fa-user-tag',
-			'label_color' => 'success',
-		],
-		['header' => 'cyber'],
-		[
-			'text' => 'computers',
-			'url' => 'admin/computers',
-			'icon' => 'fas fa-tv',
-		],
-		[
-			'text' => 'Bitacora',
-			'url' => 'admin/bitacora',
-			'icon' => 'fas fa-list-alt',
-		],
+    'menu' => [
+        ['header' => 'main_navigation'],
+        [
+            'text' => 'Dashboard',
+            'url' => 'admin',
+            'icon' => 'nav-icon fas fa-tachometer-alt',
+            'label_color' => 'success',
+        ],
+        ['header' => 'administration'],
+        [
+            'text' => 'Usuarios',
+            'route' => 'admin.users.index',
+            'icon' => 'nav-icon fas fa-user-friends',
+            'label_color' => 'success',
+        ],
+        [
+            'text' => 'Roles',
+            'route' => 'admin.roles.index',
+            'icon' => 'nav-icon fas fa-user-tag',
+            'label_color' => 'success',
+        ],
+        ['header' => 'cyber'],
+        [
+            'text' => 'computers',
+            'url' => 'admin/computers',
+            'icon' => 'fas fa-tv',
+        ],
+        [
+            'text' => 'Bitacora',
+            'url' => 'admin/bitacora',
+            'icon' => 'fas fa-list-alt',
+        ],
 
-		[
-			'text' => 'multilevel',
-			'icon' => 'fas fa-fw fa-share',
-			'submenu' => [
-				[
-					'text' => 'level_one',
-					'url' => '#',
-				],
-				[
-					'text' => 'level_one',
-					'url' => '#',
-					'submenu' => [
-						[
-							'text' => 'level_two',
-							'url' => '#',
-						],
-						[
-							'text' => 'level_two',
-							'url' => '#',
-							'submenu' => [
-								[
-									'text' => 'level_three',
-									'url' => '#',
-								],
-								[
-									'text' => 'level_three',
-									'url' => '#',
-								],
-							],
-						],
-					],
-				],
-				[
-					'text' => 'level_one',
-					'url' => '#',
-				],
-			],
-		],
-		['header' => 'shop'],
-		[
-			'text' => 'products',
-			'icon' => 'fa fa-box',
-			'route' => 'admin.products.index',
-		],
-		[
-			'text' => 'categories',
-			'icon' => 'fas fa-circle',
-			'route' => 'admin.categories.index',
-		],
-		[
-			'text' => 'sell',
-			'icon' => 'fa fa-cart-plus',
-			'url' => '#',
-		],
-		[
-			'text' => 'sales',
-			'icon' => 'fa fa-list',
-			'url' => '#',
-		],
-		[
-			'text' => 'clients',
-			'icon' => 'fas fa-users',
-			'url' => '#',
-		],
-		['header' => 'labels'],
-		[
-			'text' => 'important',
-			'icon_color' => 'red',
-			'url' => '#',
-		],
-		[
-			'text' => 'warning',
-			'icon_color' => 'yellow',
-			'url' => '#',
-		],
-		[
-			'text' => 'information',
-			'icon_color' => 'cyan',
-			'url' => '#',
-		],
-	],
+        [
+            'text' => 'multilevel',
+            'icon' => 'fas fa-fw fa-share',
+            'submenu' => [
+                [
+                    'text' => 'level_one',
+                    'url' => '#',
+                ],
+                [
+                    'text' => 'level_one',
+                    'url' => '#',
+                    'submenu' => [
+                        [
+                            'text' => 'level_two',
+                            'url' => '#',
+                        ],
+                        [
+                            'text' => 'level_two',
+                            'url' => '#',
+                            'submenu' => [
+                                [
+                                    'text' => 'level_three',
+                                    'url' => '#',
+                                ],
+                                [
+                                    'text' => 'level_three',
+                                    'url' => '#',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                [
+                    'text' => 'level_one',
+                    'url' => '#',
+                ],
+            ],
+        ],
+    ],
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Menu Filters
 	|--------------------------------------------------------------------------
@@ -349,17 +307,17 @@ return [
 	|
 	*/
 
-	'filters' => [
-		JeroenNoten\LaravelAdminLte\Menu\Filters\GateFilter::class,
-		JeroenNoten\LaravelAdminLte\Menu\Filters\HrefFilter::class,
-		JeroenNoten\LaravelAdminLte\Menu\Filters\SearchFilter::class,
-		JeroenNoten\LaravelAdminLte\Menu\Filters\ActiveFilter::class,
-		JeroenNoten\LaravelAdminLte\Menu\Filters\ClassesFilter::class,
-		JeroenNoten\LaravelAdminLte\Menu\Filters\LangFilter::class,
-		JeroenNoten\LaravelAdminLte\Menu\Filters\DataFilter::class,
-	],
+    'filters' => [
+        JeroenNoten\LaravelAdminLte\Menu\Filters\GateFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\HrefFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\SearchFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\ActiveFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\ClassesFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\LangFilter::class,
+        JeroenNoten\LaravelAdminLte\Menu\Filters\DataFilter::class,
+    ],
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Plugins Initialization
 	|--------------------------------------------------------------------------
@@ -371,80 +329,80 @@ return [
 	|
 	*/
 
-	'plugins' => [
-		'Datatables' => [
-			'active' => false,
-			'files' => [
-				[
-					'type' => 'js',
-					'asset' => false,
-					'location' => '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js',
-				],
-				[
-					'type' => 'js',
-					'asset' => false,
-					'location' => '//cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js',
-				],
-				[
-					'type' => 'css',
-					'asset' => false,
-					'location' => '//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css',
-				],
-			],
-		],
-		'Select2' => [
-			'active' => false,
-			'files' => [
-				[
-					'type' => 'js',
-					'asset' => false,
-					'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js',
-				],
-				[
-					'type' => 'css',
-					'asset' => false,
-					'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css',
-				],
-			],
-		],
-		'Chartjs' => [
-			'active' => false,
-			'files' => [
-				[
-					'type' => 'js',
-					'asset' => false,
-					'location' => '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js',
-				],
-			],
-		],
-		'Sweetalert2' => [
-			'active' => false,
-			'files' => [
-				[
-					'type' => 'js',
-					'asset' => false,
-					'location' => '//cdn.jsdelivr.net/npm/sweetalert2@8',
-				],
-			],
-		],
-		'Pace' => [
-			'active' => false,
-			'files' => [
-				[
-					'type' => 'css',
-					'asset' => false,
-					'location' => '//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/themes/blue/pace-theme-center-radar.min.css',
-				],
-				[
-					'type' => 'js',
-					'asset' => false,
-					'location' => '//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js',
-				],
-			],
-		],
-	],
+    'plugins' => [
+        'Datatables' => [
+            'active' => false,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js',
+                ],
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js',
+                ],
+                [
+                    'type' => 'css',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css',
+                ],
+            ],
+        ],
+        'Select2' => [
+            'active' => false,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js',
+                ],
+                [
+                    'type' => 'css',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css',
+                ],
+            ],
+        ],
+        'Chartjs' => [
+            'active' => false,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js',
+                ],
+            ],
+        ],
+        'Sweetalert2' => [
+            'active' => false,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdn.jsdelivr.net/npm/sweetalert2@8',
+                ],
+            ],
+        ],
+        'Pace' => [
+            'active' => false,
+            'files' => [
+                [
+                    'type' => 'css',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/themes/blue/pace-theme-center-radar.min.css',
+                ],
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js',
+                ],
+            ],
+        ],
+    ],
 
-	/*
+    /*
 	|--------------------------------------------------------------------------
 	| Livewire
 	|--------------------------------------------------------------------------
@@ -455,5 +413,5 @@ return [
 	| https://github.com/jeroennoten/Laravel-AdminLTE/wiki/9.-Other-Configuration
 	*/
 
-	'livewire' => false,
+    'livewire' => false,
 ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,30 +2,22 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
-use App\Models\Product;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
-	/**
-	 * Seed the application's database.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		// \App\Models\User::factory(10)->create();
-		Storage::deleteDirectory('products');
-		Storage::makeDirectory('products');
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
 
-		$this->call(RoleSeeder::class);
-		$this->call(UserSeeder::class);
-		$this->call(ComputerSeeder::class);
-
-		Category::factory(4)->create();
-		Product::factory(100)->create();
-		$this->call(ProductSeeder::class);
-	}
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(ComputerSeeder::class);
+    }
 }
